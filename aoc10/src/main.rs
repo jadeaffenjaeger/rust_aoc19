a7e12f1767a18558d209badd8e6ba78e26c45ccb
Elias <elias_trommer@gmx.de>
2020-06-13 11:31:52 +0200

    Implement Day 10 Part 2

--- aoc10/src/main.rs
+++ aoc10/src/main.rs
@@ -15,18 +15,23 @@ fn star_coords(field: String) -> Vec<(f32, f32)> {
     coords
 }
 
-fn count_visible(stars: &Vec<(f32, f32)>, s: &(f32, f32)) -> u32 {
-    // let mut vis: u32 = stars.len() as u32 - 1;
+// Convert all stars into angle and distance representation from a given grid location 's'
+fn get_angles_dists(stars: &Vec<(f32, f32)>, s: &(f32, f32)) -> Vec<(f32, f32)> {
     let is_self = |p: &(f32, f32)| approx_eq!(f32, p.0, 0.0) && approx_eq!(f32, p.1, 0.0);
-    let mut vis: Vec<_> = stars
+    stars
         .iter()
         .map(|s1| (s1.0 - s.0, s1.1 - s.1))
         .filter(|p| !is_self(p))
-        .map(|p| p.0.atan2(p.1))
-        .collect();
-    vis.sort_by(|a,b| a.partial_cmp(b).unwrap());
-    vis.dedup_by(|a,b| approx_eq!(f32, *a,*b));
-    vis.len() as u32
+        .map(|p| (p.1.atan2(p.0), (p.0.powi(2) + p.1.powi(2)).sqrt()))
+        .collect()
+}
+
+// Retrieve number of unique angles (aka visible stars) from a given grid location
+fn count_visible(stars: &Vec<(f32, f32)>, s: &(f32, f32)) -> u32 {
+    let mut angles: Vec<_> = get_angles_dists(stars, s).iter().map(|p| p.0).collect();
+    angles.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    angles.dedup_by(|a, b| approx_eq!(f32, *a, *b));
+    angles.len() as u32
 }
 
 fn main() {
@@ -34,20 +39,55 @@ fn main() {
     let filename = &args[1];
     let contents = fs::read_to_string(filename).unwrap();
     let stars = star_coords(contents);
-    let max_visible = stars.iter().map(|s| count_visible(&stars, s)).max().unwrap();
-    println!("Solution Part 1: {}", max_visible);
+    let max_visible = stars
+        .iter()
+        .map(|s| (s, count_visible(&stars, s)))
+        .max_by_key(|s| s.1)
+        .unwrap();
+    println!("Solution Part 1: {}", max_visible.1);
+
+    let mut remaining: Vec<_> = get_angles_dists(&stars, &max_visible.0);
+
+    // Flip angles for clockwise rotation
+    remaining = remaining.iter().map(|x| (-x.0, x.1)).collect();
+
+    // Sort by distance first, then angle to encounter the nearest star at any angle first
+    remaining.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
+    remaining.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+
+    // Remove vaporized stars one by one
+    let mut i = 0;
+    loop {
+        // Vaporize but remember its angle
+        let angle = remaining[i].0;
+        remaining.remove(i);
+        i %= remaining.len();
+
+        if stars.len() - remaining.len() == 200 {
+            print_solution(remaining, max_visible.0, i);
+            break;
+        }
+
+        // Skip all further stars at the current angle
+        while approx_eq!(f32, remaining[i].0, angle) {
+            i += 1;
+            // Round complete, start next round
+            if i >= remaining.len() {
+                i = 0;
+                break;
+            }
+        }
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_count() {
-        let vec = vec![0, 1, 1, 2, 1, 0];
-        assert_eq!(count_digits(&vec, 0), 2);
-        assert_eq!(count_digits(&vec, 1), 3);
-        assert_eq!(count_digits(&vec, 2), 1);
-        assert_eq!(count_digits(&vec[0..2], 1), 1);
-    }
+fn print_solution(remaining: Vec<(f32, f32)>, location: &(f32, f32), idx: usize) {
+    // Recover cartesian offset from angle and distance representation
+    let offset = (
+        ((-remaining[idx].0).cos() * remaining[idx].1).round(),
+        ((-remaining[idx].0).sin() * remaining[idx].1).round(),
+    );
+    println!(
+        "Solution Part 2: {}",
+        (location.1 + offset.1) * 100.0 + location.0 + offset.0
+    );
 }
